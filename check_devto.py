c88efdd5940d31e725992fceaf271fcda1f7a501
Mr. Dogood <mrdogood@amr.dev>
2026-04-07 00:51:56 -0400

    Remove utility scripts from repo, add to .gitignore

--- check_devto.py
+++
@@ -1,4 +0,0 @@
-import requests
-r = requests.get('https://dev.to/api/articles?username=realmrmemory', headers={'api-key': 'uRut7ESDweUDGwXd5TPsQWPM'})
-for a in r.json():
-    print(f"{a['published_at'][:10]} | {a['title'][:70]} | {a['url']}")
